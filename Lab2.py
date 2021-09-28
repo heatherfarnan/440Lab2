0d15280fb6cf2867eaac16ecd578ebad7725770b
heatherfarnan <91498628+heatherfarnan@users.noreply.github.com>
2021-09-28 17:46:08 +0000

    trying to get the first bullet. so added switch 1 and triangle wave code

--- Lab2.py
+++ Lab2.py
@@ -9,29 +9,30 @@ gpio.setmode(gpio.BCM)
 gpio.setup(in1, gpio.IN, pull_up_down=gpio.PUD_DOWN)
 gpio.setup(in2, gpio.IN, pull_up_down=gpio.PUD_DOWN)
 
-
+#define output port numbers:
 p1 = 26
 p2 = 4
 p3 = 21
-
-
 
 gpio.setup(p1, gpio.OUT)
 gpio.setup(p2, gpio.OUT)
 gpio.setup(p3, gpio.OUT)
 
 
-pwm = gpio.PWM(p1, 100)          # create PWM object @ 100 Hz
+pwm1 = gpio.PWM(p1, 1)          # create PWM object @ 1 Hz
+pwm2 = gpio.PWM(p1, 1)          # create PWM object @ 1 Hz
+pwm3 = gpio.PWM(p1, 1)          # create PWM object @ 1 Hz
 
-while in1 == 1:
-  try:
-    pwm.start(0)                  # initiate PWM at 0% duty cycle
-    while 1:
-      for dc in range(101):       # loop duty cycle from 0 to 100  
-        pwm.ChangeDutyCycle(dc)   # set duty cycle  
-        sleep(0.01)               # sleep 10 ms
-  except KeyboardInterrupt:       # stop gracefully on ctrl-C
-    print('\nExiting')
+
+
+try:
+  while(in1 == 1):
+    for dc1 in range(101):
+      pwm.ChangeDutyCycle(dc1)
+    for dc1 in range(101,0,-1):
+      pwm.ChangeDutyCycle(dc1)
+except KeyboardInterrupt:       # stop gracefully on ctrl-C
+  print('\nExiting')
 
 pwm.stop()
 gpio.cleanup()
