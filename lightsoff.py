3071518bb12587ddfbdd8ab69c6a5ccbdb4afc7b
heatherfarnan <91498628+heatherfarnan@users.noreply.github.com>
2021-09-28 18:24:57 +0000

    added lightsoff.py

---
+++ lightsoff.py
@@ -0,0 +1,14 @@
+import RPi.GPIOas gpio
+
+gpio.setmode(gpio.BCM)
+
+p1 = 26
+p2 = 4
+p3 = 21
+
+try
+  gpio.output(p1, 0)
+  gpio.output(p2, 0)
+  gpio.output(p3, 0)
+except KeyboardInterrupt:
+  print('\nExiting')
