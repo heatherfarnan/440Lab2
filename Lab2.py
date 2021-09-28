abe2e4205d54a52491f11104aa14501a30bc6d85
heatherfarnan <91498628+heatherfarnan@users.noreply.github.com>
2021-09-28 18:05:02 +0000

    added callback.py, added a blink to Lab2

--- Lab2.py
+++ Lab2.py
@@ -15,6 +15,14 @@ p2 = 4
 p3 = 21
 
 gpio.setup(p1, gpio.OUT)
+gpio.setup(p2, gpio.OUT)
+gpio.setup(p3, gpio.OUT)
+
+while True:             # continuous loop
+  GPIO.output(p3, 0)     # set output to 0V
+  sleep(0.5)            # wait 0.5 sec
+  GPIO.output(p3, 1)     # set output to 3.3V
+  sleep(0.5)            # wait 0.5 sec
 
 def myCallback(pin):
   p1 = 1
@@ -22,3 +30,8 @@ def myCallback(pin):
 gpio.add_event_detect(in1, gpio.RISING, callback=myCallback, bouncetime=100)
   
 gpio.cleanup()
+
+GPIO.setmode(GPIO.BCM)  # use BCM port numbering
+p = 4                   # pin number
+GPIO.setup(p, GPIO.OUT) # assign the pin as output
+
