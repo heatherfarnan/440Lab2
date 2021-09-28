12e4b864b0ca3dbf948819924353f111eb2e4de7
heatherfarnan <91498628+heatherfarnan@users.noreply.github.com>
2021-09-28 18:21:46 +0000

    fixed blink freq, got rid of callback while loops

--- Lab2.py
+++ Lab2.py
@@ -19,19 +19,17 @@ gpio.setup(p2, gpio.OUT)
 gpio.setup(p3, gpio.OUT)
 
 
-def myCallback(pin):
-  while(gpio.input(in1)==1):  
-    gpio.output(p1,1)
-  while(gpio.input(in2)==1):  
-    gpio.output(p2,1)
+def myCallback(pin):  
+  gpio.output(p1,1)
+  gpio.output(p2,1)
 
 gpio.add_event_detect(in1, gpio.RISING, callback=myCallback, bouncetime=1000)
 
 while True:             # continuous loop
   gpio.output(p3, 0)     # set output to 0V
-  sleep(0.5)            # wait 0.5 sec
+  sleep(1)            # wait 0.5 sec
   gpio.output(p3, 1)     # set output to 3.3V
-  sleep(0.5)   
+  sleep(1)   
 
 gpio.cleanup()
 
